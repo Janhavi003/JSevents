2923b5349cbc97f06cf812cbf276297d6139fca2
Janhavi003 <144115237+Janhavi003@users.noreply.github.com>
2023-10-23 16:49:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+const user = document.getElementById('user');
+
+function onloading() {
+  user.innerHTML = 'Janhavi';
+}
+
+const btn = document.getElementById('btn-click');
+btn.addEventListener('click', function onClick(event) {
+  btn.style.backgroundColor = 'lightblue';
+});
+
+const btn3 = document.getElementById('build-button');
+
+function makeSentence() {
+  const noun = document.getElementById('noun');
+  const verb = document.getElementById('verb');
+  const adverb = document.getElementById('adverb');
+  const result = document.getElementById('statement');
+
+  a = noun.value;
+  b = verb.value;
+  c = adverb.value;
+
+  d = a + ' ' + b + ' ' + c;
+  console.log(d);
+  result.innerHTML = d;
+}
+
+btn3.addEventListener('click', makeSentence);
+
+const grandparent1 = document.getElementById('grandparent');
+const parent1 = document.getElementById('parent');
+const child1 = document.getElementById('child');
+
+grandparent1.addEventListener(
+  'click',
+  function () {
+    console.log('GrandParent clicked');
+  },
+  false
+);
+
+parent1.addEventListener(
+  'click',
+  function () {
+    console.log('Parent clicked');
+  },
+  false
+);
+
+child1.addEventListener(
+  'click',
+  function () {
+    console.log('Child clicked');
+  },
+  false
+);
+
+const grandparent2 = document.getElementById('grandparent');
+const parent2 = document.getElementById('parent');
+const child2 = document.getElementById('child');
+
+grandparent2.addEventListener(
+  'click',
+  function () {
+    console.log('GrandParent clicked');
+  },
+  true
+);
+
+parent2.addEventListener(
+  'click',
+  function () {
+    console.log('Parent clicked');
+  },
+  true
+);
+
+child2.addEventListener(
+  'click',
+  function () {
+    console.log('Child clicked');
+  },
+  true
+);
+
+const category = document.getElementById('category');
+
+category.addEventListener('click', (e) => {
+  if (e.target.id == 'blazers') {
+    console.log('blazers');
+  }
+});
